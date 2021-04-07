22171eb5d29876945acf0ac3092226a861e9cedd
Azat <razatr@yandex.ru>
2021-04-07 11:53:03 +0300

    added the ability to set the grid size

--- src/Chart/lab6.js
+++ src/Chart/lab6.js
@@ -2,12 +2,14 @@ function f(x) {
     return -x * x * x - 2
 }
 
+const n = 30
+
 function f2(x, y) {
     return 3 * x * x / (x * x * x + y + 1)
 }
 
 function mEuler(i, x) {
-    return fEuler[i - 1]['y'] + ( x - (i - 1) / 30) * f2((i - 1) / 30, fEuler[i - 1]['y'])
+    return fEuler[i - 1]['y'] + ( x - (i - 1) / n) * f2((i - 1) / n, fEuler[i - 1]['y'])
 }
 
 export const func = []
@@ -17,7 +19,7 @@ export const secondOrder2 = [{ x:0, y:-2}]
 export const fourthOrder = [{ x:0, y:-2}]
 
 for (let i = 0; i < 50; i++) {
-    let x = i / 30
+    let x = i / n
     func.push({
         x: x,
         y: f(x)
@@ -25,18 +27,18 @@ for (let i = 0; i < 50; i++) {
 }
 
 for (let i = 1; i <= 30; i++) {
-    const x = i / 30
+    const x = i / n
     fEuler.push({
         x: x,
         y: mEuler(i, x)
     })
     secondOrder1.push({
         x: x,
-        y: fEuler[i - 1]['y'] + (f2(x,f(x)) + f2((i - 1) / 30, fEuler[i - 1]['y'])) / 60
+        y: fEuler[i - 1]['y'] + (f2(x,f(x)) + f2((i - 1) / n, fEuler[i - 1]['y'])) / (2 * n)
     })
 
-    const k1 = f2((i - 1) / 30, fEuler[i - 1]['y']) / 30
-    const k2 = f2((i - 1) / 30 + 1 / 30, fEuler[i - 1]['y'] + k1) / 30
+    const k1 = f2((i - 1) / n, fEuler[i - 1]['y']) / n
+    const k2 = f2((i - 1) / n + 1 / n, fEuler[i - 1]['y'] + k1) / n
 
     secondOrder2.push({
         x: x,
@@ -45,12 +47,12 @@ for (let i = 1; i <= 30; i++) {
 }
 
 for (let i = 1; i <= 30; i++) {
-    const x = i / 30
+    const x = i / n
 
-    const k1 = f2((i - 1) / 30, fEuler[i - 1]['y']) / 30
-    const k2 = f2((i - 1) / 30 + 1 / 60, fEuler[i - 1]['y'] + k1 / 2) / 30
-    const k3 = f2((i - 1) / 30 + 1 / 60, fEuler[i - 1]['y'] + k2 / 2) / 30
-    const k4 = f2((i - 1) / 30 + 1 / 30, fEuler[i - 1]['y'] + k3) / 30
+    const k1 = f2((i - 1) / n, fEuler[i - 1]['y']) / n
+    const k2 = f2((i - 1) / n + 1 / (2 * n), fEuler[i - 1]['y'] + k1 / 2) / n
+    const k3 = f2((i - 1) / n + 1 / (2 * n), fEuler[i - 1]['y'] + k2 / 2) / n
+    const k4 = f2((i - 1) / n + 1 / n, fEuler[i - 1]['y'] + k3) / n
 
     fourthOrder.push({
         x: x,
